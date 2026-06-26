1b2663623b491192b3fc5c92f2fdb14b719f76bd
zequinha-taveira <80562960+zequinha-taveira@users.noreply.github.com>
2026-06-25 22:08:35 -0300

    Add missing androidx.compose.foundation.background import
    Fixes unresolved reference error for .background() modifier calls used in:
    - Line 177: Box with transparent white background
    - Line 369: Box with horizontal gradient background

--- android/app/src/main/java/com/sonix/player/MainActivity.kt
+++ android/app/src/main/java/com/sonix/player/MainActivity.kt
@@ -3,6 +3,7 @@ package com.sonix.player
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
